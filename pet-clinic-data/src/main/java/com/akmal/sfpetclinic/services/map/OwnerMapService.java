a3f7caf01c834dc7eea67f5ab0c584eb9abc694c
akmal2409 <akmalalikhujaev@gmail.com>
2020-06-27 18:11:39 +0200

    Added MapService. Closes #6

---
+++ pet-clinic-data/src/main/java/com/akmal/sfpetclinic/services/map/OwnerMapService.java
@@ -0,0 +1,33 @@
+package com.akmal.sfpetclinic.services.map;
+
+import com.akmal.sfpetclinic.model.Owner;
+import com.akmal.sfpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerMapService extends AbstractMapService<Owner, Long> implements CrudService<Owner,Long> {
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner owner) {
+        return super.save(owner.getId(), owner);
+    }
+
+    @Override
+    public void delete(Owner owner) {
+        super.delete(owner);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
---
+++ pet-clinic-data/src/main/java/com/akmal/sfpetclinic/services/map/PetMapService.java
@@ -0,0 +1,33 @@
+package com.akmal.sfpetclinic.services.map;
+
+import com.akmal.sfpetclinic.model.Pet;
+import com.akmal.sfpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetMapService extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return super.save(pet.getId(), pet);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        super.delete(pet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
