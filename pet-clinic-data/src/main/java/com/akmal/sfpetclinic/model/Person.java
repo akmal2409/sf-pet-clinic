ce476cc1e6c6c4eac735bf9ec499e62294a40ca1
akmal2409 <akmalalikhujaev@gmail.com>
2020-07-06 14:48:04 +0200

    Converted Owner entity into JPA. Closes #36

--- pet-clinic-data/src/main/java/com/akmal/sfpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/akmal/sfpetclinic/model/Person.java
@@ -1,8 +1,15 @@
 package com.akmal.sfpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass // Because it inherits and we wont be working with Person Pojo
 public class Person extends BaseEntity {
 
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
 
     public String getFirstName() {
