15480ac3a3435f8e98d8deefd28d42852e9bbd8b
akmal2409 <akmalalikhujaev@gmail.com>
2020-07-27 17:55:12 +0200

    Implemented Visit Controller

--- pet-clinic-web/src/main/java/com/akmal/sfpetclinic/controllers/VisitController.java
+++ pet-clinic-web/src/main/java/com/akmal/sfpetclinic/controllers/VisitController.java
@@ -21,5 +21,68 @@ import javax.validation.Valid;
 @RequestMapping("owners/{ownerId}/pets/{petId}")
 public class VisitController {
 
+    private final PetService petService;
+    private final VisitService visitService;
 
+    public VisitController(PetService petService, VisitService visitService) {
+        this.petService = petService;
+        this.visitService = visitService;
+    }
+
+    @ModelAttribute("visit")
+    public Visit loadVisits(@PathVariable Long petId, Model model){
+
+        Pet requestedPet = petService.findById(petId);
+        model.addAttribute("pet", requestedPet);
+        Visit visit = new Visit();
+        requestedPet.getVisits().add(visit);
+        visit.setPet(requestedPet);
+
+        return visit;
+    }
+
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder){
+        dataBinder.setDisallowedFields("id");
+    }
+
+    @GetMapping("/visits/new")
+    public String createVisitForm(){
+
+        return "pets/createOrUpdateVisitForm";
+    }
+
+    @PostMapping("/visits/new")
+    public String processCreateForm(@Valid Visit visit, BindingResult result,
+                                    @PathVariable Long ownerId, @PathVariable Long petId){
+
+        if(result.hasErrors()){
+            return "pets/createOrUpdateVisitForm";
+        } else{
+            Visit savedVisit = visitService.save(visit);
+
+            return "redirect:/owners/" + ownerId;
+        }
+    }
+
+    @GetMapping("/visits/{visitId}/edit")
+    public String updateVisitForm(@PathVariable Long visitId, Model model){
+
+        model.addAttribute("visit", visitService.findById(visitId));
+
+        return "pets/createOrUpdateVisitForm";
+    }
+
+    @PostMapping("/visits/{visitId}/edit")
+    public String processUpdateForm(@Valid Visit visit, BindingResult result, Owner owner){
+
+        if(result.hasErrors()){
+            return "pets/createOrUpdateVisitForm";
+        } else{
+
+            visitService.save(visit);
+
+            return "redirect:/owners/" + owner.getId();
+        }
+    }
 }
